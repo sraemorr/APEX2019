357351d916698ccda773e687e099f45efc62094c
ApexMeatball <MRA_M123@outlook.com>
2019-01-19 14:42:29 -0800

    Update src/main/java/frc/robot/subsystems/DriveTrain.java
    new drivetrain subsystem

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,10 @@
+package main.java.frc.robot.subsystems; 
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem{
+
+
+    public void initDefaultCommand(){
+        
+    }
+}
