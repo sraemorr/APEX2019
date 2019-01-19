bac75618ca3e7985f35ad3b15a5041211c3dbb31
DriveraStation2\Apex5803 <steph@apexfrc.com>
2019-01-18 17:49:34 -0800

    built in source control works let's have a round of applause for those brave developers. eclipse is dead long live the new gods may eclipse never see the light of day again

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -58,7 +58,6 @@ public class Robot extends TimedRobot {
    * You can use it to reset any subsystem information you want to clear when
    * the robot is disabled.
    */
-  //yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeehaaaaw
   @Override
   public void disabledInit() {
   }
