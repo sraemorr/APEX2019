bb4f508494beee7e7a2f6b48af40c4fa34541e3d
ApexMeatball <MRA_M123@outlook.com>
2019-01-21 15:48:53 -0800

    my comptuer is dumb lol

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,9 +1,9 @@
-package frc.robot.subsystems; 
+package main.java.frc.robot.subsystems; 
 import  edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lift extends Subsystem{
 
     public void initDefaultCommand(){
-        
+
     }
 }
