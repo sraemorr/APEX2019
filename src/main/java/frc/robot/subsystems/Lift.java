7c1ab27a285af56335814f344a4a6dd0e347371f
ApexMeatball <MRA_M123@outlook.com>
2019-01-21 18:02:05 -0800

    Update src/main/java/frc/robot/subsystems/Lift.java

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -2,8 +2,8 @@ package main.java.frc.robot.subsystems;
 import  edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Lift extends Subsystem{
-
+    
     public void initDefaultCommand(){
-        
+
     }
 }
