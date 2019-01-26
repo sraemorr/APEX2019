384b1e246b04a0189b72709cccef5794f6003ada
ApexMeatball <MRA_M123@outlook.com>
2019-01-26 13:42:16 -0800

    Update src/main/java/frc/robot/OI.java
    Worked on OI a bit more; still needs more inputs and the actual button choices but that's for later now

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,39 +7,26 @@
 
 package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
+
+import org.graalvm.compiler.replacements.nodes.ReverseBytesNode;
+import org.usfirst.frc.team5803.robot.utils.POVTrigger;
+import org.usfirst.frc.team5803.robot.utils.TriggerButton;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.Trigger;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.EatCargo;
+import frc.robot.commands.ForwardCrossbow;
+import frc.robot.commands.MoveLift;
+import frc.robot.commands.MoveLiftManual;
+import frc.robot.commands.ReverseCrossbow;
+import frc.robot.commands.SpitCargo;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
-
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
-
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
-
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
-
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
-
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
-
     public static XboxController xbox1; 
     public static XboxController xbox2; 
 
@@ -47,6 +34,29 @@ public class OI {
       xbox1 = new XboxController(0);
       xbox2 = new XboxController(1);
 
+      Button RPT2=new JoystickButton(xbox2, 1 );  
+      RPT2.whileHeld(new MoveLiftManual());
+      Button LPT2=new JoystickButton(xbox2, 2 );  
+      LPT2.whileHeld(new MoveLift());
+      
+      Button LPB2=new JoystickButton(xbox2, 3 );  //Change this to appropriate paddle value after testing
+      LPB2.whenActive(new MoveLiftManual());
+      LBB2.whenInactive();
+
+      Button LB2=new JoystickButton(xbox2,5);
+        	LB2.whileHeld(new EatCargo());
+
+      Button RB1=new JoystickButton(xbox2,6);
+          RB1.whileHeld(new SpitCargo());
+      Button RB2=new JoystickButton(xbox2,6);
+          RB2.whileHeld(new SpitCargo());
+
+      Button Y1=new JoystickButton(xbox1,4);
+        	Y1.whileActive(new ForwardCrossbow());
+        	Y1.whenInactive(new ReverseCrossbow());
+      Button Y2=new JoystickButton(xbox2,4);
+         	Y2.whileActive(new ForwardCrossbow());
+           Y2.whenInactive(new ReverseCrossbow());
     }
 
     public XboxController xbox1(){
