ebf92fb3539b56f2149c34548c025ac98b665a23
Jonathan Schaller <JJ.latanom42@gmail.com>
2019-01-28 16:42:36 -0800

    added ramp commands

--- src/main/java/frc/robot/commands/ExtendRamp.java
+++ src/main/java/frc/robot/commands/ExtendRamp.java
@@ -1,1 +1,32 @@
-package frc.robot.commands;
+package frc.robot.commands; 
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.Crossbow;
+import frc.robot.RobotMap;
+
+public class ExtendRamp extends Command{
+    public ExtendRamp(){
+       
+    }
+
+    protected void initialize(){
+       
+    }
+
+    protected void execute(){
+        Robot.cargoIntake.ExtendRamp();
+    }
+
+    protected boolean isFinished(){
+        return false; 
+    }
+
+    protected void end(){
+
+    }
+
+    protected void interrupted(){
+        end();
+    }
+}
--- src/main/java/frc/robot/commands/RetractRamp.java
+++ src/main/java/frc/robot/commands/RetractRamp.java
@@ -1,1 +1,32 @@
-package frc.robot.commands;
+package frc.robot.commands; 
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.Crossbow;
+import frc.robot.RobotMap;
+
+public class RetractRamp extends Command{
+    public RetractRamp(){
+       
+    }
+
+    protected void initialize(){
+       
+    }
+
+    protected void execute(){
+        Robot.cargoIntake.RetractRamp();;
+    }
+
+    protected boolean isFinished(){
+        return false; 
+    }
+
+    protected void end(){
+
+    }
+
+    protected void interrupted(){
+        end();
+    }
+}
