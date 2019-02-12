b56a19b39c75655cc4ca56d3551534c577764657
ApexMeatball <MRA_M123@outlook.com>
2019-02-11 19:30:14 -0800

    added Scheduler, more dashboard code in SmartDashConfig

--- src/main/java/frc/robot/utils/SmartDashConfig.java
+++ src/main/java/frc/robot/utils/SmartDashConfig.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.can.*;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import com.ctre.phoenix.sensors.PigeonIMU;
@@ -16,10 +17,13 @@ public class SmartDashConfig {
         SmartDashboard.putNumber("R1 encoder position", RobotMap.R1.getSelectedSensorPosition(0));
         SmartDashboard.putNumber("RobotAngle", 50. * Robot.driveTrain.getAngle());
 
+        //SmartDashboard.putNumber("Lift 1 error", angle - Lift1.getSelectedSensorPosition(0)*360/4096);
+
+        Scheduler.getInstance().run();
     }
 
     public static void Comp() {
         //SmartDashboard.putData("Auto mode", chooser);
-        
+        Scheduler.getInstance().run();
     }
 }
