efa30d474677b48f6ffaa845e28de0dec990f6b7
ApexMeatball <MRA_M123@outlook.com>
2019-02-15 14:27:36 -0800

    added intake variable speed

--- src/main/java/frc/robot/subsystems/CargoIntake.java
+++ src/main/java/frc/robot/subsystems/CargoIntake.java
@@ -5,7 +5,9 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.*;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.OI;
 import frc.robot.RobotMap;
 
 
@@ -16,10 +18,34 @@ public class CargoIntake extends Subsystem {
         RobotMap.Intake.set(ControlMode.PercentOutput, -0.5);
     }
 
+    public void IntakeManual(){
+        if(OI.xbox2.getTriggerAxis(Hand.kLeft) >= 0.2){
+            Intake.set(ControlMode.PercentOutput, 0.5 * OI.xbox2.getTriggerAxis(Hand.kLeft));
+        }
+        else if(OI.xbox2.getTriggerAxis(Hand.kLeft) >= -0.2){
+            Intake.set(ControlMode.PercentOutput, 0.5 * OI.xbox2.getTriggerAxis(Hand.kLeft));
+        }
+        else {
+            end();
+        }
+    }
+
     public void Expel(){
         RobotMap.Intake.set(ControlMode.PercentOutput, 0.5 );
        
 
+    }
+
+    public void ExpelManual(){
+        if(OI.xbox2.getTriggerAxis(Hand.kLeft) >= 0.2){
+            Intake.set(ControlMode.PercentOutput, -0.5 * OI.xbox2.getTriggerAxis(Hand.kRight));
+        }
+        else if(OI.xbox2.getTriggerAxis(Hand.kRight) >= -0.2){
+            Intake.set(ControlMode.PercentOutput, -0.5 * OI.xbox2.getTriggerAxis(Hand.kRight));
+        }
+        else{
+            end();
+        }
     }
 
  
